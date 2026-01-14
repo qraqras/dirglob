db9d45756f3b0eb646da88effcfb1bbe6c125d83
qraqras <q100rrraaa@gmail.com>
2026-01-14 10:47:16 +0000

    tmp

--- check_ruby.rb
+++
@@ -1,8 +0,0 @@
-
-Dir.chdir("../tests/fixtures") do
-  puts "p0103 (**/?):"
-  puts Dir.glob("**/?").sort
-  puts "---"
-  puts "p0118 (**/**/):"
-  puts Dir.glob("**/**/").sort
-end
--- verify_p0103.rb
+++
@@ -1,9 +0,0 @@
-
-Dir.chdir("../tests/fixtures") do
-  res = Dir.glob("**/?").sort
-  puts "Count: #{res.count}"
-  res.each { |x| puts x }
-end
-puts "---"
-puts "FNM_DOTMATCH check:"
-puts File.fnmatch("?", ".")
